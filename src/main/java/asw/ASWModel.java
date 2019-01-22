def284776afd2d8deb237032950a4496fbc42b56
WZ.Dai <daviiz1117@163.com>
2019-01-22 17:06:02 +0800

    base version

---
+++ src/main/java/asw/ASWModel.java
@@ -0,0 +1,51 @@
+package asw;
+
+
+import java.rmi.RemoteException;
+
+import nl.tudelft.simulation.dsol.SimRuntimeException;
+import nl.tudelft.simulation.dsol.logger.SimLogger;
+import nl.tudelft.simulation.dsol.model.AbstractDSOLModel;
+import nl.tudelft.simulation.dsol.simulators.DEVSSimulatorInterface;
+
+/**
+ * <p>
+ * Copyright (c) 2002-2019 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
+ * project is distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
+ * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
+ * </p>
+ */
+public class ASWModel extends AbstractDSOLModel.TimeDouble<DEVSSimulatorInterface.TimeDouble>
+{
+    /** The default serial version UID for serializable classes. */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * constructs a new BallModel.
+     * @param simulator the simulator
+     */
+    public ASWModel(final DEVSSimulatorInterface.TimeDouble simulator)
+    {
+        super(simulator);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void constructModel() throws SimRuntimeException
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            try
+            {
+                new DiscreteBall(this.simulator);
+            }
+            catch (RemoteException exception)
+            {
+                SimLogger.always().error(exception);
+            }
+        }
+    }
+
+}
